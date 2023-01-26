a50b903991e36e3ad3f39dba699b480adc68c7c4
teiqn <teikoresawa@gmail.com>
2023-01-26 15:21:42 +0900

    working looping div creator.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
+
+for (x=0; x<256; x++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    box.textContent = "test";
+    document.getElementById("container").appendChild(box);
+} 
+
+const box = document.getElementsByClassName("box");
+
+box.addEventListener("mouseover", (event) => {
+    box.classList.add("playing")
+});
