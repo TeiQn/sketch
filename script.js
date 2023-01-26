2daaf7b7ebf803f2a1d9311d34d8e677e4494333
teiqn <teikoresawa@gmail.com>
2023-01-26 16:52:02 +0900

    Working version of Etch a Sketch.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const default_d = 16
+const default_c = "black"
+
 const container = document.querySelector("#container");
 
-for (x=0; x<256; x++) {
+const grid = document.createElement("div");
+grid.id = "grid";
+
+container.appendChild(grid);
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function setupGrid(size){
+for (let x = 0; x < size * size; x++) {
     const box = document.createElement("div");
-    box.className = "box";
-    box.textContent = "test";
-    document.getElementById("container").appendChild(box);
-} 
+    box.addEventListener("mouseover", changeColor);
+    box.addEventListener('mousedown', changeColor);
+    grid.appendChild(box);
+}
+};
 
-const box = document.getElementsByClassName("box");
+function changeColor(e) {
+    if (e.type === "mouseover" && !mouseDown) return
+    e.target.style.backgroundColor = "red"
+}
 
-box.addEventListener("mouseover", (event) => {
-    box.classList.add("playing")
-});
+window.onload = () => {
+    setupGrid(default_d)
+}
